75f2328abba3d97b6cf9fc2ff59ca1ea0e89d9a1
arraybuffer <arraybuffer@foxmail.com>
2017-09-07 00:08:16 +0800

    热门搜索

--- github/pages/CustomKeyPage.js
+++ github/pages/CustomKeyPage.js
@@ -3,6 +3,7 @@ import {
   View,
   Text,
   Image,
+  ScrollView,
   RefreshControl,
   TouchableOpacity,
   StyleSheet
@@ -13,6 +14,9 @@ import ScrollableTabView, {
 
 import NavigationBar from '../components/NavigationBar';
 import PopularTab from '../components/PopularTab';
+import BackButton from '../components/BackButton';
+
+import Language, {FLAG} from '../expand/Language';
 
 /**
  * backButtonIcon: 返回按钮图标
@@ -32,35 +36,89 @@ export default class CustomKeyPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      key: '',
-      text: '',
-      repositoryList: ''
+      dataList: [],
     };
+    this.language = new Language(FLAG.key)
   }
 
-  getButton(image) {
-    return (
-      <TouchableOpacity onPress={() => {
-        this.props.navigator.pop()
-      }}>
-        <Image style={iconStyle} source={image}/>
-      </TouchableOpacity>
-    )
+  // 获取keys
+  loadData() {
+    this.language.fetch()
+      .then(result => {
+        this.setState({
+          dataList: result
+        })
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+  componentDidMount() {
+    this.loadData()
+  }
+
+  rightButtonTapHandler() {
+
+  }
+
+  renderTagList() {
+    if (!this.state.dataList || this.state.dataList.length === 0) return null;
+    let data = this.state.dataList,
+      len = this.state.dataList.length,
+      views = [];
+    for (let i = 0, l = len - 2; i < l; i += 2) {
+      views.push(
+        <View key={i}>
+          <View style = { styles.row }>
+            <Text>{ data[i].name }</Text>
+            <Text>{ data[i+1].name }</Text>
+          </View>
+          <View style = { styles.line }/>
+        </View>
+      )
+    }
+
+    views.push(
+      <View key={len - 1}>
+        <View style = { styles.row }>
+          { len % 2 === 0 ? <Text>{ data[len-2].name }</Text> : null}
+          <Text>{ data[len-1].name }</Text>
+        </View>
+        <View style = { styles.line }/>
+      </View>
+    );
+    return views;
   }
 
   render() {
 
+    // 顶部导航条右边按钮
+    let rightButton = (
+      <TouchableOpacity>
+        <View style={styles.rightButton}>
+          <Text style={styles.rightButtonText}>保存</Text>
+        </View>
+      </TouchableOpacity>
+    );
+
+    // 导航条
     let navigationBar = (
       <NavigationBar
         title={'自定义标签'}
-        statusBar={{backgroundColor: '#2196F3'}}
-        leftButton={this.getButton(backButtonIcon)}
-        rightButton={this.getButton(starButtonIcon)}
+        leftButton={<BackButton callback={() => {
+          this.props.navigator.pop()
+        }}/>}
+        rightButton={rightButton}
       />
     );
 
     return <View style={styles.container}>
-      { navigationBar }
+      {navigationBar}
+      <ScrollView>
+        <Text>自定义标识</Text>
+        {this.renderTagList()}
+      </ScrollView>
     </View>
   }
 }
@@ -81,4 +139,18 @@ const styles = StyleSheet.create({
   statusBar: {
     backgroundColor: '#2196F3'
   },
+  rightButton: {
+    margin: 5
+  },
+  rightButtonText: {
+    color: 'white'
+  },
+  line: {
+    height: 1,
+    backgroundColor: 'black'
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  }
 });
