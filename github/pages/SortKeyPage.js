ee980e698cd42b8f70e9b11e1d74b9157efaae78
arraybuffer <arraybuffer@foxmail.com>
2017-10-19 23:35:35 +0800

    sort

--- github/pages/SortKeyPage.js
+++ github/pages/SortKeyPage.js
@@ -2,11 +2,17 @@ import React, {Component} from 'react';
 import {
   View,
   Text,
-  StyleSheet
+  Image,
+  Alert,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableHighlight
 } from 'react-native';
 import Language, {FLAG} from '../expand/Language';
 import SortableListView from 'react-native-sortable-listview';
 import NavigationBar from '../components/NavigationBar';
+import BackButton from '../components/BackButton';
+import Utils from '../utils/CommonUtils';
 export default class SortKeyPage extends Component {
   constructor(props) {
     super(props);
@@ -27,7 +33,7 @@ export default class SortKeyPage extends Component {
     this.lan.fetch()
       .then(res => {
         this.getCheckedKeys(res);
-        this.data = data;
+        this.allKeyArr = res;
       })
       .catch(res => {
       })
@@ -43,19 +49,74 @@ export default class SortKeyPage extends Component {
     this.originCheckedArr.push(...checkedArr);
   }
 
+  onBack () {
+    if (Utils.isEqualArr(this.originCheckedArr, this.state.checkedArr)) {
+      this.props.navigator.pop();
+    } else {
+      Alert.alert(
+        '提示',
+        '是否保存修改',
+        [{
+          text: '否',
+          onPress: () => {
+            this.props.navigator.pop();
+          }
+        }, {
+          text: '是',
+          onPress: () => {
+            this.onSaveData(true);
+          }
+        }]
+      )
+    }
+  }
+
+  getSortResult () {
+    this.sortResultArray = Utils.cloneArr(this.allKeyArr);
+    for (let i = 0, j = this.originCheckedArr.length; i < j; i++) {
+      let item = this.originCheckedArr[i];
+      let index = this.allKeyArr.indexOf(item);
+      this.sortResultArray.splice(index, 1, this.state.checkedArr[i]);
+    }
+  }
+
+  onSaveData (isChcked) {
+    if (isChcked && Utils.isEqualArr(this.state.checkedArr, this.originCheckedArr)) {
+      this.props.navigator.pop();
+    } else {
+      this.getSortResult();
+      this.lan.save(this.sortResultArray);
+      this.props.navigator.pop();
+    }
+  }
+
   render () {
+
+    // 顶部导航条右边按钮
+    let rightButton = (
+      <TouchableOpacity>
+        <View style={styles.rightButton}>
+          <Text onPress = { () => this.onSaveData() } style={styles.rightButtonText}>保存</Text>
+        </View>
+      </TouchableOpacity>
+    );
+
     return(
       <View style = { styles.container }>
-        <NavigationBar title = '我的'/>
+        <NavigationBar 
+          title = '我的'
+          leftButton = { <BackButton callback = {() => this.onBack()}/> }
+          rightButton = { rightButton }
+        />
         <SortableListView
           style={{ flex: 1 }}
           data={ this.state.checkedArr }
           order={ Object.keys(this.state.checkedArr) }
           onRowMoved={e => {
-            order.splice(e.to, 0, this.state.checkedArr.splice(e.from, 1)[0]);
+            this.state.checkedArr.splice(e.to, 0, this.state.checkedArr.splice(e.from, 1)[0]);
             this.forceUpdate()
           }}
-          renderRow={row => <SortCell data={row} />}
+          renderRow={row => <SortCell data={ row }  {...this.props} />}
         />
       </View>
     )
@@ -68,9 +129,25 @@ class SortCell extends Component {
   }
   render(){
     return(
-      <View>
-        <Text>{ this.props.data.name }</Text>
-      </View>
+      <TouchableHighlight
+        underlayColor = { '#eee' }
+        underlayPress = { 500 }
+        style = { styles.sortRow }
+        {...this.props.sortHandlers}
+      >
+        <View style = {{
+          flexDirection: 'row',
+          alignItems: 'center'
+        }}>
+          <Image source={ require('../image/ic_sort.png')} style={{
+            tintColor: '#2169f3',
+            height: 16,
+            width: 16,
+            marginRight: 10
+          }}/>
+          <Text>{ this.props.data.name }</Text>
+        </View>
+      </TouchableHighlight>
     )
   }
 }
@@ -78,5 +155,17 @@ class SortCell extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1
-  }
+  },
+  sortRow: {
+    padding: 25,
+    backgroundColor: '#f8f8f8',
+    borderBottomWidth: 1,
+    borderColor: '#eee'
+  },
+  rightButtonText: {
+    color: 'white'
+  },
+  rightButton: {
+    margin: 5
+  },
 });
--- github/utils/CommonUtils.js
+++ github/utils/CommonUtils.js
@@ -9,4 +9,23 @@ export default class Utils {
     arr.push(item);
     return arr;
   }
+  static isEqualArr (arr1, arr2) {
+    if(!(arr1 && arr2)) return false;
+    if(!(arr1.length === arr2.length)) return false;
+    let result = true;
+    for(let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        result = false;
+        break;
+      }
+    }
+    return result;
+  }
+  static cloneArr (arr1) {
+    let  arr2 = [];
+    for (let i = 0; i < arr1.length; i++) {
+      arr2.push(arr1[i]);
+    }
+    return arr2;
+  }
 }
