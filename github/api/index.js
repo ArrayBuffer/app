9de0f90798f2002d6b8488c706d21ca823d30093
arraybuffer <arraybuffer@foxmail.com>
2018-02-23 17:39:49 +0800

    first commit

--- github/api/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * products：获取产品
- * @type {{products: string}}
- */
-export const api = {
-  products: 'http://39.108.138.156/api/products/',
-  getRepository: 'https://api.github.com/search/repositories'
-};
--- github/index.android.js
+++
@@ -1,4 +0,0 @@
-import { AppRegistry } from 'react-native';
-import App from './pages/App';
-
-AppRegistry.registerComponent('github', () => App);
